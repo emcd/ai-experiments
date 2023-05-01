41b9696d588df40dfeb9525bdf30f1cca720f5d1
Eric <emcd@users.noreply.github.com>
2023-04-30 20:14:54 -0700

    Initial population.

---
+++ create-venv.bash
@@ -0,0 +1,6 @@
+declare -r venv_name='langchain-venv'
+rm --force --recursive "${venv_name}"
+python3 -m venv "${venv_name}"
+source "${venv_name}/bin/activate"
+python3 -m pip install --upgrade pip
+python3 -m pip install langchain notebook openai
