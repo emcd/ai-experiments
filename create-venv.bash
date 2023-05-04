1dd1a01994dc54ccbc45563c0173ba6e78c7eb3b
Eric <emcd@users.noreply.github.com>
2023-05-03 19:41:56 -0700

    Add Langchain chat.

--- create-venv.bash
+++ create-venv.bash
@@ -1,6 +1,7 @@
+set -eu -o pipefail
 declare -r venv_name='langchain-venv'
 rm --force --recursive "${venv_name}"
 python3 -m venv "${venv_name}"
 source "${venv_name}/bin/activate"
 python3 -m pip install --upgrade pip
-python3 -m pip install langchain notebook openai
+python3 -m pip install langchain notebook openai tomli
