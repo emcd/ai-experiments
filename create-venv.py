62aaf3b98f88e3f7a5b262aacbd8a910acafe2f5
Eric McDonald <emcd@users.noreply.github.com>
2023-05-13 12:29:02 -0700

    Create working Panel app (with assist from GPT 4) to query local FAISS vector DB.

--- create-venv.py
+++ create-venv.py
@@ -4,30 +4,31 @@ from pathlib import Path
 from subprocess import run
 from venv import create as create_venv
 
-base_path = Path( __file__ ).parent.resolve( )
-local_path = base_path / '.local'
+base_location = Path( __file__ ).parent.resolve( )
+local_location = base_location / '.local'
 for dirname in ( 'environments', 'installations', 'utilities' ):
-    ( local_path / dirname ).mkdir( exist_ok = True, parents = True )
+    ( local_location / dirname ).mkdir( exist_ok = True, parents = True )
 
-venv_path = local_path / 'environments/langchain'
+venv_location = local_location / 'environments/langchain'
 
-if not venv_path.is_dir( ): create_venv( str( venv_path ), with_pip = True )
+if not venv_location.is_dir( ):
+    create_venv( str( venv_location ), with_pip = True )
 
-def activate_virtual_env( venv_path ):
-    from os import environ, pathsep
-    new_env = environ.copy()
-    new_env[ 'VIRTUAL_ENV' ] = str( venv_path )
-    new_env[ 'PATH' ] = pathsep.join(
-        ( str( venv_path / 'bin' ), new_env[ 'PATH' ] ) )
-    new_env.pop( 'PYTHONHOME', None )
-    return new_env
+def activate_virtual_env( venv_location ):
+    import os
+    process_environment = os.environ.copy( )
+    process_environment[ 'VIRTUAL_ENV' ] = str( venv_location )
+    process_environment[ 'PATH' ] = os.pathsep.join(
+        ( str( venv_location / 'bin' ), process_environment[ 'PATH' ] ) )
+    process_environment.pop( 'PYTHONHOME', None )
+    return process_environment
 
-venv_env = activate_virtual_env( venv_path )
+venv_variables = activate_virtual_env( venv_location )
 
 run(
     ( 'python3', '-m', 'pip', 'install', '--upgrade', 'pip' ),
-    env = venv_env )
-requirements_file = base_path / '.local/configuration/requirements.pip'
+    env = venv_variables )
+requirements_location = base_location / '.local/configuration/requirements.pip'
 run(
-    ( 'python3', '-m', 'pip', 'install', '-r', str( requirements_file ) ),
-    env = venv_env )
+    ( 'python3', '-m', 'pip', 'install',
+      '--requirement', str( requirements_location ) ), env = venv_variables )
--- panel-query.py
+++ panel-query.py
@@ -1,17 +1,57 @@
-''' Simple GUI built with Panel to support vectorstore and LLM queries. '''
+import panel as pn
+from langchain.vectorstores.faiss import FAISS
+from pathlib import Path
+from pickle import load
 
+def load_vectorstore():
+    vectorstore_path = Path('vectorstore.pypickle')
+    with vectorstore_path.open('rb') as file:
+        vectorstore = load(file)
+    return vectorstore
 
-def layout_gui( number_of_results ):
+def run_query(event, text_input_query, rows_results, vectorstore):
+    query = text_input_query.value
+    docs = vectorstore.similarity_search(query)
+    for i, doc in enumerate(docs[:len(rows_results)]):
+        rows_results[i][1].object = doc.page_content
+
+def layout_gui(number_of_results, vectorstore):
     from panel import Column, Row
     from panel.pane import Markdown
     from panel.widgets import Button, TextInput
     text_input_query = TextInput(
-        value = '', placeholder = 'Enter query here...' )
-    button_run = Button( name = 'Run Query' )
+        value='', placeholder='Enter query here...')
+    button_run = Button(name='Run Query')
     rows_results = tuple(
-        Row( f"{i}", Markdown( '', width = 640 ) )
-        for i in range( number_of_results ) )
+        Row(f"{i}", Markdown('', width=640))
+        for i in range(number_of_results))
+    button_run.on_click(
+        lambda event: run_query(
+            event, text_input_query, rows_results, vectorstore) )
     dashboard = Column(
         text_input_query,
         button_run,
-        *rows_results )
+        *rows_results)
+    return dashboard
+
+
+def provide_credentials( ):
+    import tomli as tomllib
+    with open(
+        '.local/configuration/credentials.toml', 'rb'
+    ) as credentials_file: credentials = tomllib.load(credentials_file)
+    return dict(
+        openai_api_key = credentials['openai']['token'],
+        openai_organization = credentials['openai']['organization'] )
+
+def main():
+    import openai
+    openai_credentials = provide_credentials( )
+    openai.api_key = openai_credentials[ 'openai_api_key' ]
+    openai.organization = openai_credentials[ 'openai_organization' ]
+    vectorstore = load_vectorstore()
+    dashboard = layout_gui(number_of_results=5, vectorstore=vectorstore)
+    pn.serve(dashboard, start=True)
+
+if __name__ == "__main__":
+    main()
