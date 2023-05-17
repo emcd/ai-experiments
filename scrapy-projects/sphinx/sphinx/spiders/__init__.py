b6c6f3bc942d09e794960b27b2ac0a93fac2e84d
Eric McDonald <emcd@users.noreply.github.com>
2023-05-16 18:11:49 -0700

    Strip 'canvas', 'nav', and 'script' tags from scraped pages to maximize relevance of chunked text fed into vectorstore. (Co-authored with GPT-4.)

--- scrapy-projects/sphinx/sphinx/spiders/__init__.py
+++ scrapy-projects/sphinx/sphinx/spiders/__init__.py
@@ -1,7 +1,9 @@
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
 
+from bs4 import BeautifulSoup
 from scrapy.linkextractors import LinkExtractor
+#from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider, Rule
 
 from sphinx.items import SphinxItem
@@ -12,12 +14,12 @@ class SphinxSpider( CrawlSpider ):
     name = 'sphinx'
     allowed_domains = (
         'docs.scrapy.org',
-        #'panel.holoviz.org',
+        'panel.holoviz.org',
         'python.langchain.com',
     )
     start_urls = (
         'https://docs.scrapy.org/en/latest',
-        #'https://panel.holoviz.org/api/index.html',
+        'https://panel.holoviz.org/api/index.html',
         'https://python.langchain.com/en/latest/',
     )
     rules = (
@@ -32,11 +34,15 @@ class SphinxSpider( CrawlSpider ):
         item[ 'url' ] = response.url
         title = response.css( 'h1::text' ).get( )
         item[ 'title' ] = title.strip( ) if title else None
-        content = response.css( '.body' ).get( )
+        soup = BeautifulSoup( response.body, 'html.parser' )
+        for unwanted_tag in soup( ( 'canvas', 'nav', 'script', ) ):
+            unwanted_tag.decompose( )
+        for toctree_class in (
+            'toctree-wrapper', 'toctree-l1', 'toctree-l2', 'toctree-l3'
+        ):
+            for toctree_element in soup.select( f'.{toctree_class}' ):
+                toctree_element.decompose( )
+        content_fragments = soup.body.stripped_strings
+        content = '\n'.join( content_fragments )
         item[ 'content' ] = content.strip( ) if content else None
-        content_fragments = response.xpath(
-            '//body//text()[normalize-space()]' ).getall( )
-        content = '\n'.join(
-            fragment.strip( ) for fragment in content_fragments )
-        item['content'] = content.strip( ) if content else None
         return item
