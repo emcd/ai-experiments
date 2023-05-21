1aecf0e25478b38ce9b76887eee1f632d697defb
Eric McDonald <emcd@users.noreply.github.com>
2023-05-20 18:23:21 -0700

    GUI: Perform OpenAI chat completion.

--- panel-query.py
+++ panel-query.py
@@ -1,37 +1,64 @@
-import panel as pn
-from langchain.vectorstores.faiss import FAISS
-from pathlib import Path
-from pickle import load
-
-def load_vectorstore():
-    vectorstore_path = Path('vectorstore.pypickle')
-    with vectorstore_path.open('rb') as file:
-        vectorstore = load(file)
+def load_vectorstore( ):
+    from pathlib import Path
+    from pickle import load
+    vectorstore_path = Path( 'vectorstore.pypickle' )
+    with vectorstore_path.open( 'rb' ) as file: vectorstore = load( file )
     return vectorstore
 
-def run_query(event, text_input_query, rows_results, vectorstore):
-    query = text_input_query.value
-    docs = vectorstore.similarity_search(query)
-    for i, doc in enumerate(docs[:len(rows_results)]):
-        rows_results[i][1].object = doc.page_content
 
-def layout_gui(number_of_results, vectorstore):
+def create_prompt( query, docs, initial_system_message ):
+    prompt = [ initial_system_message ]
+    prompt.append(
+        "Additionally, you have the following pieces of supplemental "
+        "information available to you to help you accurately respond:" )
+    for i, doc in enumerate( docs ):
+        prompt.append( f"## Supplement {i + 1}\n{doc.page_content}" )
+    system_message = '\n\n'.join( prompt )
+    messages = [
+        { 'role' : 'system', 'content' : system_message },
+        { 'role' : 'user', 'content' : query },
+    ]
+    return messages
+
+
+def run_query( event, layout_items, vectorstore ):
+    from openai import ChatCompletion
+    query = layout_items[ 'text_input_user' ].value
+    system_message = layout_items[ 'text_input_system' ].value
+    rows_results = layout_items[ 'rows_results' ]
+    docs = vectorstore.similarity_search( query, k = len( rows_results ) )
+    for i, doc in enumerate( docs ):
+        rows_results[ i ][ 1 ].object = doc.page_content
+    messages = create_prompt( query, docs, system_message )
+    response = ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = messages,
+        temperature = 0, )
+    layout_items[ 'token_counter' ].value = response.usage[ 'total_tokens' ]
+    layout_items[ 'answer_display' ].object = (
+        response.choices[ 0 ].message[ 'content' ] )
+
+
+def layout_gui( vectorstore, number_of_results ):
     from panel import Column, Row
     from panel.pane import Markdown
-    from panel.widgets import Button, TextInput
-    text_input_query = TextInput(
-        value='', placeholder='Enter query here...')
-    button_run = Button(name='Run Query')
-    rows_results = tuple(
-        Row(f"{i}", Markdown('', width=640))
-        for i in range(number_of_results))
-    button_run.on_click(
-        lambda event: run_query(
-            event, text_input_query, rows_results, vectorstore) )
-    dashboard = Column(
-        text_input_query,
-        button_run,
-        *rows_results)
+    from panel.widgets import Button, IntInput, TextInput
+    layout_items = dict(
+        text_input_user = TextInput(
+            value = '', placeholder = 'Enter user message here...' ),
+        text_input_system = TextInput(
+            value = '', placeholder = 'Enter system message here...' ),
+        button_run = Button( name = 'Run Query' ),
+        rows_results = Column(
+            *(Row( f"{i}", Markdown( '', width = 640, ) )
+              for i in range( number_of_results ) ) ),
+        token_counter = IntInput(
+            name = 'Token Counter', value = 0, disabled = True, ),
+        answer_display = Markdown( '', width = 640, ),
+    )
+    dashboard = Column( *layout_items.values( ) )
+    layout_items[ 'button_run' ].on_click(
+        lambda event : run_query( event, layout_items, vectorstore ) )
     return dashboard
 
 
@@ -39,19 +66,22 @@ def provide_credentials( ):
     import tomli as tomllib
     with open(
         '.local/configuration/credentials.toml', 'rb'
-    ) as credentials_file: credentials = tomllib.load(credentials_file)
+    ) as credentials_file: credentials = tomllib.load( credentials_file )
     return dict(
-        openai_api_key = credentials['openai']['token'],
-        openai_organization = credentials['openai']['organization'] )
+        openai_api_key = credentials[ 'openai' ][ 'token' ],
+        openai_organization = credentials[ 'openai' ][ 'organization' ] )
 
-def main():
+
+def main( ):
     import openai
+    import panel
     openai_credentials = provide_credentials( )
     openai.api_key = openai_credentials[ 'openai_api_key' ]
     openai.organization = openai_credentials[ 'openai_organization' ]
-    vectorstore = load_vectorstore()
-    dashboard = layout_gui(number_of_results=5, vectorstore=vectorstore)
-    pn.serve(dashboard, start=True)
+    vectorstore = load_vectorstore( )
+    dashboard = layout_gui(
+        vectorstore = load_vectorstore( ), number_of_results = 3 )
+    panel.serve( dashboard, start = True )
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == "__main__": main( )
