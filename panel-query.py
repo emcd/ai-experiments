51c8c49ee93fbd2defe5d1991efcb2bae47b00f2
Eric McDonald <emcd@users.noreply.github.com>
2023-05-29 17:20:54 -0700

    GUI: Add model selection and temperature slider. Improve conversation layout.

--- panel-query.py
+++ panel-query.py
@@ -3,18 +3,39 @@ def add_to_conversation( role, content, gui_items, kind = '' ):
     from panel.pane import Markdown
     from panel.widgets import Checkbox, StaticText
     conversation_history = gui_items[ 'center' ][ 'conversation_history' ]
-    checkbox = Checkbox( value = True )
-    checkbox.param.watch(
-        lambda event: update_token_count( gui_items ), 'value' )
     if 'supplement' == kind:
         content = f'''## Supplement ##\n\n{content}'''
+        emoji = '📄'
         message_cell = StaticText(
-            sizing_mode = 'stretch_both', value = content )
+            value = content,
+            height_policy = 'fit',
+            sizing_mode = 'stretch_width',
+            style = { 'background-color': 'WhiteSmoke' } )
     else:
-        message_cell = Markdown( content, sizing_mode = 'stretch_both' )
-    message_cell.metadata = { 'tokens': count_tokens( content ) }
+        if 'AI' == role:
+            emoji = '🤖'
+            style = {
+                'border-left': '2px solid LightGray',
+                'border-right': '2px solid LightGray',
+                'padding': '3px',
+            }
+        else:
+            emoji = '🧑'
+            style = { 'background-color': 'White' }
+        message_cell = Markdown(
+            content,
+            height_policy = 'fit',
+            sizing_mode = 'stretch_width',
+            style = style )
+    message_cell.metadata = {
+        'role': role,
+        'token_count': count_tokens( content ),
+    }
+    checkbox = Checkbox( name = emoji, value = True, width = 40 )
+    checkbox.param.watch(
+        lambda event: update_token_count( gui_items ), 'value' )
     new_item = Row(
-        Column( f"**{role}**", checkbox ),
+        checkbox,
         message_cell,
         # TODO: Copy button, edit button, etc...
     )
@@ -43,13 +64,19 @@ def create_dashboard( gui_items ):
         )
     )
 
+
+SYSTEM_MESSAGE_AWARE_MODELS = frozenset( ( 'gpt-4', ) )
 def generate_messages( gui_items ):
     system_message = gui_items[ 'center' ][ 'text_input_system' ].value
-    # TODO: Set role for system message according to chat model.
-    messages = [ { 'role' : 'user', 'content' : system_message } ]
+    model = gui_items[ 'right' ][ 'selector_model' ].value
+    if model not in SYSTEM_MESSAGE_AWARE_MODELS: role = 'user'
+    else: role = 'system'
+    messages = [ { 'role': role, 'content': system_message } ]
     for item in gui_items[ 'center' ][ 'conversation_history' ]:
-        if not item[ 0 ][ 1 ].value: continue # if checkbox is not checked
-        role = 'user' if item[ 0 ][ 0 ].object == 'Human' else 'assistant'
+        if not item[ 0 ].value: continue # if checkbox is not checked
+        role = (
+            'user' if item[ 1 ].metadata[ 'role' ] in ( 'Human', 'Document' )
+            else 'assistant' )
         content = item[ 1 ].object
         messages.append( { 'role': role, 'content': content } )
     return messages
@@ -57,8 +84,8 @@ def generate_messages( gui_items ):
 
 def layout_gui( ):
     from panel import Column
-    from panel.widgets import Button, IntInput, IntSlider
-    from panel.widgets.input import TextAreaInput
+    from panel.widgets import Button, FloatSlider, IntSlider, Select
+    from panel.widgets.input import StaticText, TextAreaInput
     gui_items = {
         'left': {
             'conversations_index': Column( ),
@@ -78,13 +105,20 @@ def layout_gui( ):
                 placeholder = 'Enter user message here...', value = '' ),
         },
         'right': {
+            'selector_model': Select(
+                name = 'Model',
+                options = [ 'gpt-3.5-turbo' ], value = 'gpt-3.5-turbo' ),
+            'slider_temperature': FloatSlider(
+                name = 'Temperature',
+                start = 0, end = 2, step = 0.1, value = 0 ),
+            # TODO: Move buttons to bottom of center panel.
             'button_chat': Button( name = 'Chat' ),
             'button_query': Button( name = 'Query' ),
             'slider_documents_count': IntSlider(
                 name = 'Number of Documents',
                 start = 0, end = 5, step = 1, value = 3 ),
-            'token_counter': IntInput(
-                name = 'Token Counter', value = 0, disabled = True ),
+            'token_counter': StaticText( name = 'Token Counter', value = '0' ),
+            'status': StaticText( name = 'Status', value = 'OK' ),
         },
     }
     return create_dashboard( gui_items ), gui_items
@@ -105,9 +139,20 @@ def main( ):
     openai.api_key = openai_credentials[ 'openai_api_key' ]
     openai.organization = openai_credentials[ 'openai_organization' ]
     dashboard, gui_items = layout_gui( )
+    populate_models_selection( gui_items )
     vectorstore = load_vectorstore( )
     register_gui_callbacks( gui_items, vectorstore )
     panel.serve( dashboard, start = True )
+
+
+def populate_models_selection( gui_items ):
+    # TODO: Use API-appropriate call.
+    from operator import itemgetter
+    import openai
+    models = sorted( map(
+        itemgetter( 'id' ),
+        openai.Model.list( ).to_dict_recursive( )[ 'data' ] ) )
+    gui_items[ 'right' ][ 'selector_model' ].options = models
 
 
 def provide_credentials( ):
@@ -132,20 +177,28 @@ def register_gui_callbacks( gui_items, vectorstore ):
 
 
 def run_chat( event, gui_items ):
-    from openai import ChatCompletion
+    gui_items[ 'right' ][ 'status' ].object = 'OK'
+    from openai import ChatCompletion, OpenAIError
     query = gui_items[ 'center' ][ 'text_input_user' ].value
     if query:
         add_to_conversation( 'Human', query, gui_items )
         gui_items[ 'center' ][ 'text_input_user' ].value = ''
     messages = generate_messages( gui_items )
-    response = ChatCompletion.create(
-        messages = messages, model = 'gpt-3.5-turbo', temperature = 0 )
-    add_to_conversation(
-        'AI', response.choices[ 0 ].message[ 'content' ], gui_items )
+    try:
+        response = ChatCompletion.create(
+            messages = messages,
+            model = gui_items[ 'right' ][ 'selector_model' ].value,
+            temperature = gui_items[ 'right' ][ 'slider_temperature' ].value )
+        add_to_conversation(
+            'AI', response.choices[ 0 ].message[ 'content' ].strip( ),
+            gui_items )
+    except OpenAIError as exc:
+        gui_items[ 'right' ][ 'status' ].object = f"Error: {exc}"
     update_token_count( gui_items )
 
 
 def run_query( event, gui_items, vectorstore ):
+    gui_items[ 'right' ][ 'status' ].object = 'OK'
     query = gui_items[ 'center' ][ 'text_input_user' ].value
     if not query: return
     add_to_conversation( 'Human', query, gui_items )
@@ -162,14 +215,14 @@ def run_query( event, gui_items, vectorstore ):
 def update_token_count( gui_items ):
     total_tokens = 0
     for item in gui_items[ 'center' ][ 'conversation_history' ]:
-        left, message_cell = item
-        _, checkbox = left
-        if checkbox.value: total_tokens += message_cell.metadata[ 'tokens' ]
+        checkbox, message_cell = item
+        if checkbox.value:
+            total_tokens += message_cell.metadata[ 'token_count' ]
     total_tokens += count_tokens(
         gui_items[ 'center' ][ 'text_input_user' ].value )
     total_tokens += count_tokens(
         gui_items[ 'center' ][ 'text_input_system' ].value )
-    gui_items[ 'right' ][ 'token_counter' ].value = total_tokens
+    gui_items[ 'right' ][ 'token_counter' ].value = str( total_tokens )
 
 
 if __name__ == "__main__": main( )
