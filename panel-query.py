125ce5f76efbc01ea9af842acdef3db96a6bf68d
Eric McDonald <emcd@users.noreply.github.com>
2023-06-04 15:18:56 -0700

    GUI: Select canned system prompts with adjustable variables.

--- panel-query.py
+++ panel-query.py
@@ -49,7 +49,7 @@ def count_tokens( content ):
 
 SYSTEM_MESSAGE_AWARE_MODELS = frozenset( ( 'gpt-4', ) )
 def generate_messages( gui ):
-    system_message = gui.text_input_system.value
+    system_message = gui.text_system_prompt.object
     model = gui.selector_model.value
     if model not in SYSTEM_MESSAGE_AWARE_MODELS: role = 'user'
     else: role = 'system'
@@ -95,13 +95,14 @@ def main( ):
     dashboard = layout_gui( gui, prepare_gui_layout( ), 'dashboard' )
     gui = SimpleNamespace( **gui )
     populate_models_selection( gui )
+    populate_system_prompts_selection( gui )
     vectorstore = load_vectorstore( )
     register_gui_callbacks( gui, vectorstore )
     panel.serve( dashboard, start = True )
 
 
 def populate_models_selection( gui ):
-    # TODO: Use API-appropriate call.
+    # TODO: Use provider-appropriate call.
     from operator import itemgetter
     import openai
     models = sorted( map(
@@ -110,11 +111,28 @@ def populate_models_selection( gui ):
     gui.selector_model.options = models
 
 
+def populate_system_prompts_selection( gui ):
+    from pathlib import Path
+    from yaml import safe_load
+    metadata = { }; prompt_names = [ ]
+    for prompt_path in Path( 'prompts' ).glob( '*.yaml' ):
+        with prompt_path.open( ) as file:
+            contents = safe_load( file )
+            id_ = contents[ 'id' ]
+            metadata[ id_ ] = contents
+            prompt_names.append( id_ )
+    gui.selector_system_prompt.metadata__ = metadata
+    gui.selector_system_prompt.options = prompt_names
+
+
 def prepare_gui_layout( ):
     from panel import Column, Row
     from panel.layout import HSpacer
-    from panel.widgets import Button, FloatSlider, IntSlider, Select
-    from panel.widgets.input import StaticText, TextAreaInput
+    from panel.pane import Markdown
+    from panel.widgets import (
+        Button, Checkbox, FloatSlider, IntSlider, Select, StaticText, TextInput,
+    )
+    from panel.widgets.input import TextAreaInput
     return {
         'dashboard': dict(
             component_class = Row,
@@ -134,6 +152,14 @@ def prepare_gui_layout( ):
                 'column_conversations_index',
             ],
         ),
+        'button_new_conversation': dict(
+            component_class = Button,
+            component_arguments = dict(
+                name = 'New Conversation',
+                width_policy = 'min',
+            ),
+        ),
+        'column_conversations_index': dict( component_class = Column ),
         'left_spacer': dict( component_class = HSpacer ),
         'center_pane': dict(
             component_class = Column,
@@ -147,6 +173,87 @@ def prepare_gui_layout( ):
                 'row_actions',
             ],
         ),
+        'row_system_prompt': dict(
+            component_class = Row,
+            contains = [ 'label_system', 'column_system_prompt', ],
+        ),
+        'label_system': dict(
+            component_class = StaticText,
+            component_arguments = dict( value = '💬📏', width = 40, ),
+        ),
+        'column_system_prompt': dict(
+            component_class = Column,
+            contains = [
+                'row_system_prompt_selector',
+                'row_system_prompt_variables',
+                'text_system_prompt',
+            ],
+        ),
+        'row_system_prompt_selector': dict(
+            component_class = Row,
+            contains = [
+                'selector_system_prompt',
+                'checkbox_display_system_prompt',
+            ],
+        ),
+        'selector_system_prompt': dict(
+            component_class = Select,
+            component_arguments = dict(
+                options = [ 'Empty', ],
+                value = 'Empty',
+            ),
+        ),
+        'checkbox_display_system_prompt': dict(
+            component_class = Checkbox,
+            component_arguments = dict(
+                align = 'center',
+                name = 'Display',
+                value = False,
+            ),
+        ),
+        'row_system_prompt_variables': dict( component_class = Row, ),
+        'text_system_prompt': dict(
+            component_class = Markdown,
+            component_arguments = dict(
+                height_policy = 'fit',
+                width_policy = 'max',
+                visible = False,
+            ),
+        ),
+        'column_conversation_history': dict(
+            component_class = Column,
+            component_arguments = dict( sizing_mode = 'stretch_both' ),
+        ),
+        'row_user_prompt': dict(
+            component_class = Row,
+            contains = [ 'label_user', 'text_input_user' ],
+        ),
+        'label_user': dict(
+            component_class = StaticText,
+            component_arguments = dict( value = '💬🧑', width = 40, ),
+        ),
+        'text_input_user': dict(
+            component_class = TextAreaInput,
+            component_arguments = dict(
+                height_policy = 'fit',
+                max_height = 480,
+                placeholder = 'Enter user message here...',
+                value = '',
+                width_policy = 'max',
+            ),
+        ),
+        'row_actions': dict(
+            component_class = Row,
+            contains = [ 'button_chat', 'button_query' ],
+        ),
+        'button_chat': dict(
+            component_class = Button,
+            component_arguments = dict( name = 'Chat' ),
+        ),
+        'button_query': dict(
+            component_class = Button,
+            component_arguments = dict( name = 'Query' ),
+        ),
         'right_spacer': dict( component_class = HSpacer ),
         'right_pane': dict(
             component_class = Column,
@@ -157,30 +264,6 @@ def prepare_gui_layout( ):
                 'text_tokens_total',
                 'text_status',
             ],
-        ),
-        'button_new_conversation': dict(
-            component_class = Button,
-            component_arguments = dict(
-                name = 'New Conversation',
-                width_policy = 'min',
-            ),
-        ),
-        'column_conversations_index': dict( component_class = Column ),
-        'row_system_prompt': dict(
-            component_class = Row,
-            contains = [ 'label_system', 'text_input_system' ],
-        ),
-        'column_conversation_history': dict(
-            component_class = Column,
-            component_arguments = dict( sizing_mode = 'stretch_both' ),
-        ),
-        'row_user_prompt': dict(
-            component_class = Row,
-            contains = [ 'label_user', 'text_input_user' ],
-        ),
-        'row_actions': dict(
-            component_class = Row,
-            contains = [ 'button_chat', 'button_query' ],
         ),
         'selector_model': dict(
             component_class = Select,
@@ -212,42 +295,6 @@ def prepare_gui_layout( ):
             component_class = StaticText,
             component_arguments = dict( name = 'Status', value = 'OK', ),
         ),
-        'label_system': dict(
-            component_class = StaticText,
-            component_arguments = dict( value = '💬📏', width = 40, ),
-        ),
-        'text_input_system': dict(
-            component_class = TextAreaInput,
-            component_arguments = dict(
-                height_policy = 'fit',
-                max_height = 480,
-                placeholder = 'Enter system message here...',
-                value = '',
-                width_policy = 'max',
-            ),
-        ),
-        'label_user': dict(
-            component_class = StaticText,
-            component_arguments = dict( value = '💬🧑', width = 40, ),
-        ),
-        'text_input_user': dict(
-            component_class = TextAreaInput,
-            component_arguments = dict(
-                height_policy = 'fit',
-                max_height = 480,
-                placeholder = 'Enter user message here...',
-                value = '',
-                width_policy = 'max',
-            ),
-        ),
-        'button_chat': dict(
-            component_class = Button,
-            component_arguments = dict( name = 'Chat' ),
-        ),
-        'button_query': dict(
-            component_class = Button,
-            component_arguments = dict( name = 'Query' ),
-        ),
     }
 
 
@@ -262,16 +309,17 @@ def provide_credentials( ):
 
 
 def register_gui_callbacks( gui, vectorstore ):
-    gui.button_chat.on_click( lambda event: run_chat( event, gui ) )
-    gui.button_query.on_click(
-        lambda event: run_query( event, gui, vectorstore ) )
-    gui.text_input_system.param.watch(
-        lambda event: update_token_count( gui ), 'value' )
+    gui.button_chat.on_click( lambda event: run_chat( gui ) )
+    gui.button_query.on_click( lambda event: run_query( gui, vectorstore ) )
+    gui.checkbox_display_system_prompt.param.watch(
+        lambda event: toggle_system_prompt_display( gui ), 'value' )
+    gui.selector_system_prompt.param.watch(
+        lambda event: update_system_prompt_variables( gui ), 'value' )
     gui.text_input_user.param.watch(
         lambda event: update_token_count( gui ), 'value' )
 
 
-def run_chat( event, gui ):
+def run_chat( gui ):
     gui.text_status.value = 'OK'
     from openai import ChatCompletion, OpenAIError
     query = gui.text_input_user.value
@@ -290,7 +338,7 @@ def run_chat( event, gui ):
     update_token_count( gui )
 
 
-def run_query( event, gui, vectorstore ):
+def run_query( gui, vectorstore ):
     gui.text_status.value = 'OK'
     query = gui.text_input_user.value
     if not query: return
@@ -305,6 +353,38 @@ def run_query( event, gui, vectorstore ):
     update_token_count( gui )
 
 
+def toggle_system_prompt_display( gui ):
+    gui.text_system_prompt.visible = gui.checkbox_display_system_prompt.value
+
+
+def update_system_prompt_text( gui ):
+    template = gui.selector_system_prompt.metadata__[
+        gui.selector_system_prompt.value ][ 'template' ]
+    variables = {
+        element.metadata__[ 'id' ]: element.value
+        for element in gui.row_system_prompt_variables
+    }
+    # TODO: Support alternative template types.
+    gui.text_system_prompt.object = template.format( **variables )
+    update_token_count( gui )
+
+
+def update_system_prompt_variables( gui ):
+    from panel.widgets import TextInput
+    gui.row_system_prompt_variables.clear( )
+    variables = gui.selector_system_prompt.metadata__[
+        gui.selector_system_prompt.value ].get( 'variables', ( ) )
+    for variable in variables:
+        # TODO: Support other widget types, such as selectors and checkboxes.
+        text_input = TextInput(
+            name = variable[ 'label' ], value = variable[ 'default' ] )
+        text_input.metadata__ = variable
+        text_input.param.watch(
+            lambda event: update_system_prompt_text( gui ), 'value' )
+        gui.row_system_prompt_variables.append( text_input )
+    update_system_prompt_text( gui )
+
+
 def update_token_count( gui ):
     total_tokens = 0
     for item in gui.column_conversation_history:
@@ -312,7 +392,7 @@ def update_token_count( gui ):
         if checkbox.value:
             total_tokens += message_cell.metadata[ 'token_count' ]
     total_tokens += count_tokens( gui.text_input_user.value )
-    total_tokens += count_tokens( gui.text_input_system.value )
+    total_tokens += count_tokens( gui.text_system_prompt.object )
     gui.text_tokens_total.value = str( total_tokens )
 
 
